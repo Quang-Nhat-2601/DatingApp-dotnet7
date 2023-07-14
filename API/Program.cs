0b7732e1bb8f76749dc4514702ae50beb8a4c87e
Nhat Le <quangnhatle2601@gmail.com>
2023-07-14 22:00:03 +0700

    update

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+
+
+var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
